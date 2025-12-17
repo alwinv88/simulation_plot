c6ce60d36f16ee5e0608b09fdbb91e2fb16b9db8
unknown <Administrator@DESKTOP-RTCQ6IO.octa.edu>
2025-12-17 19:25:25 +0530

    Added compact UI controls and download option

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from src import prepare_data, plot_graph, get_mean, read_file, apply_scaling
-
+import io
 
 st.title("Force Signal Visualization")
 
@@ -18,23 +18,56 @@ if uploaded_file:
 
     df = prepare_data(df)
 
-    st.subheader("Scaling Factor")
+    st.subheader("Plot")
 
-    factor = st.number_input(
-        "Enter multiplication factor",
-        value=1.0,        # default
-        step=0.1,
-        format="%.4f"
-    )
+    col_factor, col_x, col_y = st.columns([2, 2, 2])
+    
+    with col_factor:
+        st.caption("Scale")
+        factor = st.number_input(
+            "Scale",
+            value=1.0,
+            step=0.1,
+            format="%.4f",
+            label_visibility="collapsed"
+        )
+    
+    with col_x:
+        st.caption("X-axis")
+        x_col = st.selectbox(
+            "X-axis",
+            options=['time'],
+            label_visibility="collapsed"
+        )
+    
+    with col_y:
+        st.caption("Y-axis")
+        y_col = st.selectbox(
+            "Y-axis",
+            options=['Fx', 'Fy', 'Fz'],
+            label_visibility="collapsed"
+        )
 
-    df = apply_scaling(df, factor)
+    df = apply_scaling(df,factor=factor)
+    
+    col_plot, col_btn = st.columns([20, 1])  # wide plot, narrow button
+    with col_plot:
+        fig = plot_graph(df, x_col, y_col)
+        st.pyplot(fig)
+    
+    with col_btn:
+        buffer = io.BytesIO()
+        fig.savefig(buffer, format="png", dpi=300, bbox_inches="tight")
+        buffer.seek(0)
+    
+        st.download_button(
+            label="⬇️",                # small icon button
+            data=buffer,
+            file_name=f"{y_col}_vs_{x_col}.png",
+            mime="image/png"
+        )
 
-    # Select force component
-    y_col = st.selectbox("Select Y Component", ['Fx', 'Fy', 'Fz'])
-    x_col = st.selectbox("Select X Component", ['time']) 
 
-    fig = plot_graph(df, x_col, y_col)
-    st.pyplot(fig)
 
     st.subheader("Mean Values")
     means = get_mean(df)
