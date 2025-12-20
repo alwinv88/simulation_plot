4ba179d6de9475ac1a5e500429b9cff1d3e3a4ab
alwinv88 <alwin.varghese@tvarit.com>
2025-12-20 09:26:35 +0530

    Added the facility to upload the data from literature images.

--- app.py
+++ app.py
@@ -5,23 +5,56 @@ import io
 
 st.title("Force Signal Visualization")
 
-uploaded_file = st.file_uploader(
-    "Upload CSV or Excel file",
-    type=["csv", "xlsx", "xls"]
-)
+st.subheader("Load Data")
+col_sim, col_paper = st.columns(2)
+with col_sim:
+    st.caption("Data from Simulation")
+    file_sim = st.file_uploader(
+        "Upload the file",
+        type=["csv", "xlsx", "xls"],
+        key="sim_file",
+        label_visibility="collapsed"
+    )
 
-if uploaded_file:
-    if uploaded_file.name.endswith(".csv"):
-        df = pd.read_csv(uploaded_file)
-    else:
-        df = pd.read_excel(uploaded_file)
+with col_paper:
+    st.caption("Data from Paper")
+    file_paper = st.file_uploader(
+        "Upload the file",
+        type=["csv", "xlsx", "xls"],
+        key="paper_file",
+        label_visibility="collapsed"
+    )
 
+df = None
+data_mode = None
+default_x = ["time"]
+default_y = ["Force"]
+
+if file_sim and not file_paper:
+    df = read_file(file_sim)
     df = prepare_data(df)
+    data_mode = "sim"
 
+elif file_paper and not file_sim:
+    df = read_file(file_paper)
+    df = prepare_data(df)
+    data_mode = "paper"
+
+elif file_sim and file_paper:
+    st.warning("Please upload only one file at a time.")
+
+
+if data_mode == "sim":
+    y_options = ['Fx', 'Fy', 'Fz']
+elif data_mode == "paper":
+    y_options = ['Force']
+
+
+if df is not None:
     st.subheader("Plot")
 
     col_factor, col_x, col_y = st.columns([2, 2, 2])
-    
+
     with col_factor:
         st.caption("Scale")
         factor = st.number_input(
@@ -31,7 +64,7 @@ if uploaded_file:
             format="%.4f",
             label_visibility="collapsed"
         )
-    
+
     with col_x:
         st.caption("X-axis")
         x_col = st.selectbox(
@@ -39,27 +72,29 @@ if uploaded_file:
             options=['time'],
             label_visibility="collapsed"
         )
-    
+
     with col_y:
         st.caption("Y-axis")
         y_col = st.selectbox(
             "Y-axis",
-            options=['Fx', 'Fy', 'Fz'],
+            options=y_options if y_options else ["Select column"],
             label_visibility="collapsed"
         )
 
     df = apply_scaling(df,factor=factor)
-    
+
+
+
     col_plot, col_btn = st.columns([20, 1])  # wide plot, narrow button
     with col_plot:
         fig = plot_graph(df, x_col, y_col)
         st.pyplot(fig)
-    
+
     with col_btn:
         buffer = io.BytesIO()
         fig.savefig(buffer, format="png", dpi=300, bbox_inches="tight")
         buffer.seek(0)
-    
+
         st.download_button(
             label="⬇️",                # small icon button
             data=buffer,
@@ -71,7 +106,5 @@ if uploaded_file:
 
     st.subheader("Mean Values")
     means = get_mean(df)
-
-    st.write(f"Average Fx: {means['Fx']:.4f}")
-    st.write(f"Average Fy: {means['Fy']:.4f}")
-    st.write(f"Average Fz: {means['Fz']:.4f}")
+    for col, mean_val in means.items():
+        st.write(f"Average {col}: {mean_val:.4f}")
--- src/data_processing.py
+++ src/data_processing.py
@@ -6,29 +6,53 @@ def prepare_data(df: pd.DataFrame) -> pd.DataFrame:
     """
     df = df.copy()
 
-    df.columns = ['time', 'Fx', 'timey', 'Fy', 'timex', 'Fz']
-    df['Fx'] = -df['Fx']
-    df['Fz'] = -df['Fz']
+    
+    default_headers_sim = ['time', 'Fx', 'timey', 'Fy', 'timez', 'Fz']
+    default_headers_paper = ['time', 'Force']
+
+    if df.shape[1] == 2:
+
+        # Promote first row to header
+#         df.columns = df.iloc[0]
+#         df = df.iloc[1:].reset_index(drop=True)
+
+        # Standardize column names
+        df.columns = ['time', 'Force']
+
+    # Case 2: Existing format (Fx, Fy, Fz)
+    else:
+        df.columns = default_headers_sim
+
+        # Sign correction
+        df['Fx'] = -df['Fx']
+        df['Fz'] = -df['Fz']
+        df.drop(columns=['timey','timez'], inplace=True)
+
+    # Convert to numeric safely
+    for col in df.columns:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
 
     return df
-
 
 def get_mean(df: pd.DataFrame) -> dict:
     """
     Compute mean values of force components.
     """
-    return {
-        'Fx': df['Fx'].mean(),
-        'Fy': df['Fy'].mean(),
-        'Fz': df['Fz'].mean()
-    }
+    mean_dict = {}
+
+    for col in df.columns:
+        if col.lower() != 'time':
+            mean_dict[col] = df[col].mean()
+
+    return mean_dict
     
 def apply_scaling(df: pd.DataFrame, factor: float) -> pd.DataFrame:
     """
     Apply multiplication factor to force components.
     """
     df = df.copy()
-    df['Fx'] = df['Fx'] * factor
-    df['Fy'] = df['Fy'] * factor
-    df['Fz'] = df['Fz'] * factor
+    for col in df.columns:
+        if col.lower() != 'time':
+            df[col] = df[col] * factor
+    
     return df
